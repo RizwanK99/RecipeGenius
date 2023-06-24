f09fd7a86ed6ead7476f238912969ed067758ef0
kevinhuynh24 <kevinhuynh1224@gmail.com>
2023-06-24 18:34:43 -0400

    fixes by commenting out recyclerview

--- app/src/main/java/com/example/recipegenius/ui/myrecipes/MyRecipesFragment.java
+++ app/src/main/java/com/example/recipegenius/ui/myrecipes/MyRecipesFragment.java
@@ -13,12 +13,14 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.recipegenius.databinding.FragmentMyrecipesBinding;
 import com.example.recipegenius.ui.myrecipes.RecipeObject;
 
+import java.util.List;
+
 
 public class MyRecipesFragment extends Fragment {
 
     private FragmentMyrecipesBinding binding;
-    private RecyclerView recyclerView;
-    private CustomAdapter adapter;
+//    private RecyclerView recyclerView;
+//    private CustomAdapter adapter;
     private List<RecipeObject> recipeList;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -32,20 +34,20 @@ public class MyRecipesFragment extends Fragment {
         // create fake data of recipe objects
 
         
-        String[] ingredients = {"peanut butter", "jelly", "bread"};
-        String[] instructions = {"spread peanut butter on one slice of bread", "spread jelly on the other slice of bread", "put the two slices of bread together"};
-        String[] tags = {"nuts", "bread", "fruit"};
-        recipeList.add(new RecipeObject("Peanut Butter and Jelly", ingredients, instructions, tags));
-
-        String[] ingredients2 = {"pasta", "tomato sauce", "meatballs"};
-        String[] instructions2 = {"boil water", "cook pasta in boiling water", "heat up tomato sauce", "cook meatballs", "put pasta, tomato sauce, and meatballs together"};
-        String[] tags2 = {"pasta", "tomato", "meat"};
-        recipeList.add(new RecipeObject("Spaghetti and Meatballs", ingredients2, instructions2, tags2));
-        
-        String[] ingredients3 = {"bread", "cheese", "butter"};
-        String[] instructions3 = {"spread butter on one side of each slice of bread", "put cheese between the two slices of bread", "cook the sandwich on a pan"};
-        String[] tags3 = {"bread", "dairy"};
-        recipeList.add(new RecipeObject("Grilled Cheese", ingredients3, instructions3, tags3));
+//        String[] ingredients = {"peanut butter", "jelly", "bread"};
+//        String[] instructions = {"spread peanut butter on one slice of bread", "spread jelly on the other slice of bread", "put the two slices of bread together"};
+//        String[] tags = {"nuts", "bread", "fruit"};
+//        recipeList.add(new RecipeObject("Peanut Butter and Jelly", ingredients, instructions, tags));
+//
+//        String[] ingredients2 = {"pasta", "tomato sauce", "meatballs"};
+//        String[] instructions2 = {"boil water", "cook pasta in boiling water", "heat up tomato sauce", "cook meatballs", "put pasta, tomato sauce, and meatballs together"};
+//        String[] tags2 = {"pasta", "tomato", "meat"};
+//        recipeList.add(new RecipeObject("Spaghetti and Meatballs", ingredients2, instructions2, tags2));
+//
+//        String[] ingredients3 = {"bread", "cheese", "butter"};
+//        String[] instructions3 = {"spread butter on one side of each slice of bread", "put cheese between the two slices of bread", "cook the sandwich on a pan"};
+//        String[] tags3 = {"bread", "dairy"};
+//        recipeList.add(new RecipeObject("Grilled Cheese", ingredients3, instructions3, tags3));
 
         // // display the recipe names
         // TextView recipe1 = root.findViewById(R.id.recipe1);
--- app/src/main/java/com/example/recipegenius/ui/myrecipes/RecipeViewHolder.java
+++ app/src/main/java/com/example/recipegenius/ui/myrecipes/RecipeViewHolder.java
@@ -8,16 +8,16 @@ import com.example.recipegenius.R;
 
 import com.example.recipegenius.ui.myrecipes.RecipeObject;
 
-public class RecipeViewHolder extends RecyclerView.ViewHolder {
-    private TextView recipeName;
-    private TextView recipeIngredients;
-    private TextView recipeInstructions;
-    private TextView recipeTags;
-    View view;
-
-    public RecipeViewHolder(View itemView) {
-        super(itemView);
-        recipeName = itemView.findViewById(R.id.recipeName);
-        view = itemView;
-    }
-}
+//public class RecipeViewHolder extends RecyclerView.ViewHolder {
+//    private TextView recipeName;
+//    private TextView recipeIngredients;
+//    private TextView recipeInstructions;
+//    private TextView recipeTags;
+//    View view;
+//
+//    public RecipeViewHolder(View itemView) {
+//        super(itemView);
+//        recipeName = itemView.findViewById(R.id.recipeName);
+//        view = itemView;
+//    }
+//}
