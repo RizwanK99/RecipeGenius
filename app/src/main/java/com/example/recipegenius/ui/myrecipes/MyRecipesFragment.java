28fc5646790ee33fcee22fab6513f3d63b305c2b
kevinhuynh24 <kevinhuynh1224@gmail.com>
2023-06-24 16:37:35 -0400

    Testing out array of recipe objects

--- app/src/main/java/com/example/recipegenius/ui/myrecipes/MyRecipesFragment.java
+++ app/src/main/java/com/example/recipegenius/ui/myrecipes/MyRecipesFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.recipegenius.databinding.FragmentMyrecipesBinding;
+import com.example.recipegenius.ui.myrecipes.RecipeObject;
 
 
 public class MyRecipesFragment extends Fragment {
@@ -24,6 +25,29 @@ public class MyRecipesFragment extends Fragment {
 
         binding = FragmentMyrecipesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // create fake data of recipe objects
+        RecipeObject[] recipes = new RecipeObject[3];
+        String[] ingredients = {"peanut butter", "jelly", "bread"};
+        String[] instructions = {"spread peanut butter on one slice of bread", "spread jelly on the other slice of bread", "put the two slices of bread together"};
+        String[] tags = {"nuts", "bread", "fruit"};
+        recipes[0] = new RecipeObject("PB&J Sandwich", ingredients, instructions, tags);
+        String[] ingredients2 = {"pasta", "tomato sauce", "meatballs"};
+        String[] instructions2 = {"boil water", "cook pasta in boiling water", "heat up tomato sauce", "cook meatballs", "put pasta, tomato sauce, and meatballs together"};
+        String[] tags2 = {"pasta", "tomato", "meat"};
+        recipes[1] = new RecipeObject("Spaghetti and Meatballs", ingredients2, instructions2, tags2);
+        String[] ingredients3 = {"bread", "cheese", "butter"};
+        String[] instructions3 = {"spread butter on one side of each slice of bread", "put cheese between the two slices of bread", "cook the sandwich on a pan"};
+        String[] tags3 = {"bread", "dairy"};
+        recipes[2] = new RecipeObject("Grilled Cheese", ingredients3, instructions3, tags3);
+
+        // // display the recipe names
+        // TextView recipe1 = root.findViewById(R.id.recipe1);
+        // recipe1.setText(recipes[0].getName());
+        // TextView recipe2 = root.findViewById(R.id.recipe2);
+        // recipe2.setText(recipes[1].getName());
+        // TextView recipe3 = root.findViewById(R.id.recipe3);
+        // recipe3.setText(recipes[2].getName());
 
         final TextView textView = binding.textMyRecipes;
         myRecipesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
