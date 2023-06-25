dbab879041df045b76ea7bab10881adc8427edfd
cindyhuang19 <cindyhuang19@outlook.com>
2023-06-25 02:21:12 -0400

    two-way binding partially working, next: finish binding, save data, clean up constants, enable apply filters button

--- app/src/main/java/com/example/recipegenius/ui/filters/FiltersViewModel.java
+++ app/src/main/java/com/example/recipegenius/ui/filters/FiltersViewModel.java
@@ -4,17 +4,28 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class FiltersViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
-    public Map<String, Boolean> filters;
+    public Map<String,Boolean> filters = new HashMap<String,Boolean>();
+    public static String VEGAN = "Vegan";
+    public static String VEGETARIAN = "Vegetarian";
+    public static String KETO = "Keto";
+
+    public static String PALEO = "Paleo";
+    public static String GLUTEN_FREE = "GlutenFree";
+    public static String PESCATARIAN = "Pescatarian";
+    public static String FODMAP = "Fodmap";
 
     public FiltersViewModel() {
         mText = new MutableLiveData<>();
+        filters.put(VEGAN, true);
+        filters.put(VEGETARIAN, true);
+        filters.put(KETO, true);
     }
-
     public LiveData<String> getText() {
         return mText;
     }
